f38087b9dde6c360c11e5bab9bf0001ff831d348
Sakib <nazmunsakib81@gmail.com>
2025-08-06 22:29:06 +0600

    Restructer Database class

--- admin/includes/class.database.php
+++ admin/includes/class.database.php
@@ -9,11 +9,32 @@ class Database{
     }
 
     public function db_connection(){
-        $this->connection = mysqli_connect(DB_HOST, username: DB_USER, password: DB_PASS, database: DB_NAME);
+        //$this->connection = mysqli_connect(DB_HOST, username: DB_USER, password: DB_PASS, database: DB_NAME);
+        $this->connection = new mysqli(DB_HOST, username: DB_USER, password: DB_PASS, database: DB_NAME);
 
-        if(mysqli_connect_errno()){
-            die("Database connection failed badly" . mysqli_error());
+        if($this->connection->connect_errno){
+            die("Database connection failed badly " . $this->connection->connect_error);
         }
+    }
+
+    public function query($sql){
+        $result = $this->connection->query($sql);
+        $this->confirm($result);
+        return $result;
+    }
+
+    private function confirm($result){
+        if(!$result){
+            die("Query Faield " . $this->connection->error);
+        }
+    }
+
+    public function str_escape($string){
+        return $this->connection->real_escape_string($string);
+    }
+
+    public function inserted_id(){
+        return $this->connection->insert_id;
     }
 }
 
