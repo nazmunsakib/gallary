6033bde56c4b9e1770fd7c70a910a363f6ace4da
Sakib <nazmunsakib81@gmail.com>
2025-08-09 13:18:55 +0600

    Create session Class

---
+++ admin/includes/classes/Session.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Session {
+
+    private $signed_in = false;
+    public  $user_id;
+
+    public function __construct(){
+        session_start();
+        $this->check_the_login();
+    }
+
+    public function login($user){
+        if($user){
+            $this->user_id = $_SESSION['user_id'] = $user->id;
+            $this->signed_in = true;
+        }
+    }
+
+    public function logout(){
+        unset($_SESSION['user_id']);
+        unset($this->user_id);
+        $this->signed_in = false;
+    }
+
+    public function is_login(){
+        return $this->signed_in;
+    }
+
+    private function check_the_login(){
+        if(isset($_SESSION['user_id'])){
+            $this->user_id      = $_SESSION['user_id'];
+            $this->signed_in    = true;
+        }else{
+            unset($this->user_id);
+            $this->signed_in   = false;
+        }
+    }
+}
+
+$session = new Session();
